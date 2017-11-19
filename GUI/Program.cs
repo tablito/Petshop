63889e0048d8532653fee79e1b25d02469ffb488
Laura <vinicius.viana1992@gmail.com>
2017-11-19 18:12:32 -0200

    Alteração na mensagem da classe main

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -6,7 +6,7 @@ namespace GUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("PET SHOP");
         }
     }
 }
